fcbc958a85dcf665db1ca3fb7387cc8b83f25366
Your Name <taiirbekovag@gmail.com>
2025-05-04 13:37:54 +0600

    Multipl

---
+++ Multiple_Variables.java
@@ -0,0 +1,8 @@
+public class Multiple_Variables {
+    public static void main(String[] args) {
+        int x= 45;
+        int y = 56;
+        int z = 89;
+        System.out.println(x+y+x);
+    }
+}
