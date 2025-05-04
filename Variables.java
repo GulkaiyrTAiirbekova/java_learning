0e1ef0033fde2f19898766395f33b24817bbd199
Your Name <taiirbekovag@gmail.com>
2025-05-04 13:32:38 +0600

    Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String [] args){
+        int myNum = 12;
+        float myFloatNum = 2.99f;
+        char myLetter = 'A';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
