08c4403300996eae0b0c191540f1823b02f6d36c
Your Name <taiirbekovag@gmail.com>
2025-05-04 14:20:58 +0600

    Multiple_Values

--- Multiple_Variables.java
+++ Multiple_Variables.java
@@ -1,8 +1,17 @@
-public class Multiple_Variables {
+//public class Multiple_Variables {
+//    public static void main(String[] args) {
+//        int x= 45;
+//        int y = 56;
+//        int z = 89;
+//        System.out.println(x+y+x);
+//    }
+//}
+//One Value to Multiple Variables
+public class Multiple_Variables{
     public static void main(String[] args) {
-        int x= 45;
-        int y = 56;
-        int z = 89;
-        System.out.println(x+y+x);
+        int x, y, z;
+        x = y = z = 33;
+        System.out.println(x + y + z);
     }
 }
+
